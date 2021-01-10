d174dc1d6d403f59719c85412f679caad559b329
root <root@homeassistant.local.hass.io>
2021-01-10 17:29:00 +0000

    Automatic commit

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
-cd /config
-cp secret_files/id_rsa ~/.ssh/.
+{
+  cd /config
+  cp secret_files/id_rsa ~/.ssh/.
 
-git add .
-git commit -am "Automatic commit"
-git push
+  git add .
+  git commit -am "Automatic commit"
+  git push
+} 2>&1 | tee -a /config/backup_out.log
